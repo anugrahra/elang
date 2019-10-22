9b3af27aab5f14e4097bd99fdfca11a18689696f
Anugrah Ramadhan <dialog.anugrah@gmail.com>
2019-10-22 20:29:25 +0700

    third

---
+++ app/Classes/Barang.php
@@ -0,0 +1,25 @@
+<?php
+
+class Barang {
+  protected $kodeBarang,
+            $namaBarang,
+            $stockBarang,
+            $satuanBarang,
+            $jenisBarang;
+  
+  public function tampilkanBarang() {
+    
+  }
+  
+  public function tambahBarang() {
+
+  }
+
+  public function hapusBarang() {
+
+  }
+
+  public function editBarang() {
+
+  }
+}
