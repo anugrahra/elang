dc6458fee9498458d715bbde337f99ccebe94ad2
Anugrah Ramadhan <dialog.anugrah@gmail.com>
2019-10-24 22:00:40 +0700

    ok

---
+++ app/Classes/Petugas.php
@@ -0,0 +1,22 @@
+<?php
+
+class Petugas {
+  protected $namaPetugas,
+            $bagianPetugas;
+
+  public function tampilkanPetugas() {
+
+  }
+
+  public function tambahPetugas() {
+
+  }
+
+  public function editPetugas() {
+
+  }
+
+  public function hapusPetugas() {
+    
+  }
+}
